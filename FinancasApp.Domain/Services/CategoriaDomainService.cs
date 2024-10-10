b699ad9688668dbe1ffc4b0405b87d0f1a009c96
Yume7 <yume_owned@hotmail.com>
2024-10-10 19:43:32 -0300

    first commit

---
+++ FinancasApp.Domain/Services/CategoriaDomainService.cs
@@ -0,0 +1,16 @@
+﻿using FinancasApp.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancasApp.Domain.Services
+{
+    public class CategoriaDomainService : ICategoriaDomainService
+    {
+        public CategoriaDomainService()
+        {
+        }
+    }
+}
